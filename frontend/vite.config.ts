24544a68fa28f32b1290244a725f238a5f2ca38d
Danemiok <kenk514912@gmail.com>
2026-07-03 15:54:59 +0700

    build(frontend): update packages and vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
@@ -12,5 +13,8 @@ export default defineConfig({
   },
   server: {
     port: 5173,
+  },
+  test: {
+    globals: true,
   },
 })
