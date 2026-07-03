5fe001b07c8149ae3779e2818d9ce06e1706a890
Hutsreypov008 <eiipov465@gmail.com>
2026-07-03 15:36:30 +0700

    feat: CRUD task

--- frontend/src/services/taskService.ts
+++ frontend/src/services/taskService.ts
@@ -11,32 +11,25 @@ export const taskService = {
       params: filters,
     })
   },
-
   getOne(id: number) {
     return api.get<ApiResponse<{ task: Task }>>(`/tasks/${id}`)
   },
 
-  // TODO: implement `create`.
-  // Hint: POST to '/tasks' with the payload, same shape as
-  // categoryService.create() above. Return type should be
-  // ApiResponse<{ task: Task }>.
+  // CREATE
   create(_payload: TaskPayload) {
-    throw new Error('TODO: implement taskService.create')
+    return api.post<ApiResponse<{ task: Task}>>('/tasks', _payload)
   },
 
-  // TODO: implement `update`.
-  // Hint: PUT to `/tasks/${id}` with the payload.
+  // UPDATE & UPDATE STATUS
   update(_id: number, _payload: TaskPayload) {
-    throw new Error('TODO: implement taskService.update')
+    return api.put<ApiResponse<{ task: Task }>>(`/tasks/${_id}`, _payload)
   },
-
   updateStatus(id: number, status: Task['status']) {
     return api.patch<ApiResponse<{ task: Task }>>(`/tasks/${id}/status`, { status })
   },
 
-  // TODO: implement `remove`.
-  // Hint: DELETE `/tasks/${id}`.
+  // REMOVE
   remove(_id: number) {
-    throw new Error('TODO: implement taskService.remove')
+    return api.delete<ApiResponse<null>>(`/tasks/${_id}`)
   },
 }
